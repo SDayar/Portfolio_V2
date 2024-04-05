354ad2cd4dd8a966b4db338d373b95f47f877f47
Saifidine <sdayar318@gmail.com>
2024-04-06 00:14:34 +0200

    Modifié

---
+++ JAVASCRIPT/screen.js
@@ -0,0 +1,12 @@
+function Getscreen(){
+    X = window.screen.availWidth()
+    Y = window.screen.availHeight()
+    return X, Y
+}
+function Attribute(){
+    x = Getscreen()[0]
+    y = Getscreen()[1]
+    window.document.header.style.Height ="y"
+    window.document.header.style.width ="x"
+
+}
